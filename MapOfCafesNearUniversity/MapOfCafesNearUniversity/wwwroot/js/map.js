27d167422aa86438b6a08d3e829280ae0b06ff1c
Vladislav Bezruchko <vb0245927@gmail.com>
2025-10-18 23:03:55 +0300

    Fix: Resolve map.js build error

---
+++ MapOfCafesNearUniversity/MapOfCafesNearUniversity/wwwroot/js/map.js
@@ -0,0 +1,24 @@
+﻿let map;
+
+function initializeMap() {
+    if (map) {
+        map.remove();
+    }
+    const cafesDataElement = document.getElementById('cafes-data');
+    const cafesJson = cafesDataElement.textContent;
+    const initialCoordinates = [50.4501, 30.5234];
+    const initialZoom = 13;
+    map = L.map('map').setView(initialCoordinates, initialZoom);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const cafes = JSON.parse(cafesJson);
+
+    cafes.forEach(cafe => {
+        L.marker([cafe.latitude, cafe.longitude])
+            .addTo(map)
+            .bindPopup(cafe.popupContent);
+    });
+}
